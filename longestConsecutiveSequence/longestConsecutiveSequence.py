2e07f5fedd578d03c6122eb992218b022f06455e
russmaster76 <97500904+russmaster76@users.noreply.github.com>
2023-07-05 16:10:30 -0700

    daily

---
+++ longestConsecutiveSequence/longestConsecutiveSequence.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        containsNums = set(nums)
+        longest = 0
+        
+        for n in nums:
+            if(n-1) not in containsNums:
+                length = 0
+                while(n + length in containsNums):
+                    length += 1
+                longest = max(length, longest)
+        
+        return longest
+        
